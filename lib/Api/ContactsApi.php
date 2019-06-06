3a8db7adac8915b0961ceb40d9a6132109b925cb
Erik Räni <erik.rani@messente.com>
2019-06-06 16:25:20 +0300

    Add manual fixes

--- lib/Api/ContactsApi.php
+++ lib/Api/ContactsApi.php
@@ -2046,12 +2046,7 @@ class ContactsApi
         $multipart = false;
 
         // query params
-        if (is_array($groupIds)) {
-            $groupIds = ObjectSerializer::serializeCollection($groupIds, 'multi', true);
-        }
-        if ($groupIds !== null) {
-            $queryParams['groupIds'] = ObjectSerializer::toQueryValue($groupIds);
-        }
+        $queryParams['groupIds'] = $groupIds;
 
 
         // body params
