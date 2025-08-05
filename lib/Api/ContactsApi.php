2c3a8412bcf17f730fe3153c716d65e4525e8f6a
api-librarian <development@messente.com>
2025-08-05 08:19:11 +0000

    Travis update: Aug 2025 (Build 953)
    [skip ci]

--- lib/Api/ContactsApi.php
+++ lib/Api/ContactsApi.php
@@ -12,7 +12,7 @@
 /**
  * Messente API
  *
- * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber and WhatsApp messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
+ * [Messente](https://messente.com) is a global provider of messaging and user verification services.  * Send and receive SMS, Viber, WhatsApp and Telegram messages. * Manage contacts and groups. * Fetch detailed info about phone numbers. * Blacklist phone numbers to make sure you're not sending any unwanted messages.  Messente builds [tools](https://messente.com/documentation) to help organizations connect their services to people anywhere in the world.
  *
  * The version of the OpenAPI document: 2.0.0
  * Contact: messente@messente.com
